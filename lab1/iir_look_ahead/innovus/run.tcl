309bd3c3a4ed198a9255875d372d2c63cbacb917
wackoz <francescovacc4@gmail.com>
2021-11-11 18:42:42 +0100

    update look ahead (7bit)

--- lab1/iir_look_ahead/innovus/run.tcl
+++
@@ -1,8 +0,0 @@
-set entity iir_filter
-
-set IN_DIR "../netlist"
-set TopLevelDesign ${entity}
-set LIB_DIR /software/dk/nandgate45/liberty
-set MyTimingLib ${LIB_DIR}/NangateOpenCellLibrary_typical_ecsm_nowlm.lib
-set LEF_DIR /software/dk/nandgate45/lef
-set LEF_list [list ${LEF_DIR}/NangateOpenCellLibrary.lef]
