a182c95a2b5fe52430d197e0916dba4f33ef236f
StefanoRizzello <91958969+StefanoRizzello@users.noreply.github.com>
2021-11-15 12:16:52 +0100

    update matlab folder

--- lab1/matlab/results/thd_test.m
+++ lab1/matlab/results/thd_test.m
@@ -1,14 +1,15 @@
 clear all;
+close all;
 clc;
 
-nb=9;
-filename1= 'resultsm.txt';
-filename2= 'resultsc.txt';
-filename3= 'resultsc_8.txt';
-filename4= 'resultsc_7.txt';
+filename1= 'results_9bit.txt';
+filename2= 'results_7bit.txt';
+filename3= 'results_noopt.txt';
+filename4= 'results_opt.txt';
 delimiter= '\n';
 headerlines = 1;
 
+nb = 9;
 A1=importdata(filename1,delimiter,headerlines);
 A1.data(1)=0;
 y1=A1.data./2^(nb-1);
@@ -16,6 +17,7 @@ B1=thd(y1,10e3,9)
 figure(1)
 thd(y1,10e3,9)
 
+nb = 7;
 A2=importdata(filename2,delimiter,headerlines);
 A2.data(1)=0;
 y2=A2.data./2^(nb-1);
@@ -23,6 +25,7 @@ B2=thd(y2,10e3,9)
 figure(2)
 thd(y2,10e3,9)
 
+nb = 7;
 A3=importdata(filename3,delimiter,headerlines);
 A3.data(1)=0;
 y3=A3.data./2^(nb-1);
@@ -30,6 +33,7 @@ B3=thd(y3,10e3,9)
 figure(3)
 thd(y3,10e3,9)
 
+nb = 7;
 A4=importdata(filename4,delimiter,headerlines);
 A4.data(1)=0;
 y4=A4.data./2^(nb-1);
