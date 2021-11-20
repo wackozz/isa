43ac7151e7f3818376db43bdce8429d6df5992fe
wackoz <francescovacc4@gmail.com>
2021-11-20 19:30:45 +0100

    removed duplicates

--- lab1/iir_lookahead_nopipe/hdl/sim/simulate.sh
+++
@@ -1,4 +0,0 @@
-source /software/scripts/init_msim6.2g
-cd hdl/sim/
-vlib work
-vsim -c -do "do scripts/run_cmd.do iir_7bit_nopipe"
--- lab1/iir_lookahead_nopipe/modelsim/post_synth.sh
+++
@@ -1,4 +0,0 @@
-source /software/scripts/init_msim6.2g
-cd modelsim/
-vlib work
-vsim -c -do "do scripts/run_postsynth_cmd.do iir_filter_nopipe_postsynth"
