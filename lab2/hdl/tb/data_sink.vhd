00098e9e1d29a646282ddfb0bafa0a7290832781
StefanoRizzello <91958969+StefanoRizzello@users.noreply.github.com>
2021-11-22 18:56:17 +0100

    add tb

---
+++ lab2/hdl/tb/data_sink.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_textio.all;
+
+library std;
+use std.textio.all;
+
+entity data_sink is
+  port (
+    CLK   : in std_logic;
+    DIN   : in std_logic_vector(31 downto 0));
+end data_sink;
+
+architecture beh of data_sink is
+
+begin  -- beh
+
+  process (CLK)
+    file res_fp : text open WRITE_MODE is "../results/results_fpmul.txt";
+    variable line_out : line;    
+  begin  -- process
+    if CLK'event and CLK = '1' then  -- rising clock edge
+      hwrite(line_out, DIN);
+      writeline(res_fp, line_out);
+    end if;
+  end process;
+
+end beh;
