622655a2a690dd003788490e3d8546a3f6dd9d15
wackoz <francescovacc4@gmail.com>
2022-02-13 19:53:47 +0100

    fibonacci

--- lab3/asm/fibonacci.s
+++ lab3/asm/fibonacci.s
@@ -1,4 +1,4 @@
-	.file	"helloWorld.c"
+	.file	"fibonacci.c"
 	.option nopic
 	.attribute arch, "rv32i2p0"
 	.attribute unaligned_access, 0
@@ -15,28 +15,33 @@ main:
 	li	a5,1
 	sw	a5,-24(s0)
 	li	a5,1
-	sw	a5,-28(s0)
+	sw	a5,-32(s0)
 	lw	a4,-24(s0)
-	lw	a5,-28(s0)
+	lw	a5,-32(s0)
 	add	a5,a4,a5
 	sw	a5,-20(s0)
-	sw	zero,-32(s0)
+	sw	zero,-28(s0)
 	j	.L2
-.L4:
+.L3:
 	lw	a5,-24(s0)
-	sw	a5,-28(s0)
+	sw	a5,-32(s0)
 	lw	a5,-20(s0)
 	sw	a5,-24(s0)
 	lw	a4,-24(s0)
-	lw	a5,-28(s0)
+	lw	a5,-32(s0)
 	add	a5,a4,a5
 	sw	a5,-20(s0)
-	lw	a5,-32(s0)
+	lw	a5,-28(s0)
 	addi	a5,a5,1
-	sw	a5,-32(s0)
+	sw	a5,-28(s0)
 .L2:
+	lw	a4,-28(s0)
 	li	a5,40
-	sw	a5,-32(s0)
-	j	.L4
+	beq	a4,a5,.L3
+	li	a5,0
+	mv	a0,a5
+	lw	s0,28(sp)
+	addi	sp,sp,32
+	jr	ra
 	.size	main, .-main
 	.ident	"GCC: (GNU) 11.1.0"
